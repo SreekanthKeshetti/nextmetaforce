93bf357066494ffdfea6fd88ffe01262fc40bcbf
SreekanthKeshetti <sreekanthkeshetti9@gmail.com>
2025-11-06 16:17:25 +0530

    Prepare backend for Render deployment1

--- client/src/pages/CloudAI.jsx
+++ client/src/pages/CloudAI.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import cloudImg from "../assets/CloudAI.webp";
+import cloudImg from "../assets/CloudAIIMAGE.webp";
 
 import { FaCloud } from "react-icons/fa";
 import { Link } from "react-router-dom";
